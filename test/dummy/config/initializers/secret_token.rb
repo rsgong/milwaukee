baac4809a7f6f328d6d2dd03b46c283039e5e8cb
Timothy Cardenas <trcarden@gmail.com>
2012-04-18 13:54:39 -0700

    working assets gem, initial commit

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '0b55afe845a04315bde89017b52ca56ccfcfdf30efed246ea443d69eaa985f1fd2cc093423590f5afc5912e41f7f566858938d8d5faeb3f31001ac34f5d2d1dd'
